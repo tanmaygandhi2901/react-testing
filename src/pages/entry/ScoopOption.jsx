f2ccf4a7b1bece610fb2e2d37cc60d4ad0f9bf82
Tanmay Gandhi <tanmaygandhi@Tanmays-MacBook-Air.local>
2023-10-29 19:12:51 +0530

    Typo Errror for media loads

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -11,7 +11,7 @@ const ScoopOption = ({ name, imagePath }) => {
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
-        src={`http:localhost:3030/${imagePath}`}
+        src={`http://localhost:3030${imagePath}`}
         alt={`${name} Scoop`}
       />
       <Form.Group
